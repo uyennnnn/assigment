5669e50b3389e509c0796284ba9834801ef9afaf
uyennnnnn <buiuyen1207@gmail.com>
2022-12-16 00:26:59 +0700

    nav a

--- script.js
+++
@@ -1,8 +0,0 @@
-items = document.getElementById("additem")
-let count = 0;
-function Add(btn){
-    property = document.getElementsByClassName(btn);
-    count = count + 1;
-    items.innerHTML = "ITEMS (" + count +")";
-}
-
